d94b47fc4f27149b393ff22af15c8b9e70a05ef2
Liconeto9 <benicio.neto9@gmail.com>
2022-12-06 11:11:28 -0300

    consumindo API

--- Introducao_ao_Desenvolvimento_Web_com_Javascript/assets/js/main.js
+++ Introducao_ao_Desenvolvimento_Web_com_Javascript/assets/js/main.js
@@ -1,8 +1,32 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+function convertPokemonToLi(pokemon) {
+  return `
+            <li class="pokemon">
+                <span class="number">#001</span>
+                <span class="name">${pokemon.name}</span>
 
-fetch(url)
-  .then((response) => response.json())
-  .then((jsonBody) => console.log(jsonBody))
+                <div class="detail">
+                    <ol class="types">
+                        <li class="type">grass</li>
+                        <li class="type">poison</li>
+                    </ol>
+
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+                </div>
+                
+            </li>
+          `;
+}
+
+const pokemonList = document.getElementById("pokemonList");
+
+pokeApi
+  .getPokemons()
+  .then((pokemons) => {
+    const listItens = [];
+
+    for (let index = 0; index < pokemons.length; index++) {
+      const pokemon = pokemons[index];
+      listItens.push(convertPokemonToLi(pokemon));
+    }
+  })
   .catch((error) => console.error(error));
