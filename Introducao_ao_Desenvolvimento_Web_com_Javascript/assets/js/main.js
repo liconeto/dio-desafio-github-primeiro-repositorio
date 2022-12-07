45e27fd4a7e3cac9a99d6f557ddb827484765712
Liconeto9 <benicio.neto9@gmail.com>
2022-12-07 11:03:00 -0300

    Criando classe modelo de pokemnon e convertendo dados da Api.

--- Introducao_ao_Desenvolvimento_Web_com_Javascript/assets/js/main.js
+++ Introducao_ao_Desenvolvimento_Web_com_Javascript/assets/js/main.js
@@ -1,16 +1,19 @@
 function convertPokemonToLi(pokemon) {
   return `
-            <li class="pokemon">
-                <span class="number">#001</span>
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
                 <span class="name">${pokemon.name}</span>
 
                 <div class="detail">
                     <ol class="types">
-                        <li class="type">grass</li>
-                        <li class="type">poison</li>
+                        ${pokemon.types
+                          .map(
+                            (type) => `<li class="type ${type}">${type}</li>`
+                          )
+                          .join("")}
                     </ol>
 
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+                    <img src="${pokemon.photo}" alt="${pokemon.name}">
                 </div>
                 
             </li>
